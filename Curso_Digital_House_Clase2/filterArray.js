d067006aa8210a6d46f935a7d15f10873720b2fd
h-amarillo <harumicamarillo@gmail.com>
2021-11-08 22:29:08 -0600

    evovns

--- Curso_Digital_House_Clase2/filterArray.js
+++ Curso_Digital_House_Clase2/filterArray.js
@@ -4,11 +4,12 @@ let estudiantes = [
     {nombre: 'June', promedio: 3, aprobado: false},
 ]
 let aprobados = estudiantes.filter(function (siPasa){
-     //console.log(siPasa);
-     siPasa
-     console.log(estudiantes.promedio);
-      return  siPasa;
-      
+      return  siPasa.promedio >5;     // siPasa es una variable que guarda temporalmente un elemento del array según el índice por el que esté pasadno. Es decir que el parámetro de siPasa está e constante cambio, sin embargo en l momento que su valor es el de un indice este lo podemos m,anipular. Es por ersto que siPasa será un objeto del array el cual tiene es u interior el estado "promedio.". por esto es posible esta linea de codigo. 
 });
 console.log(aprobados);
+let desaprobados = estudiantes.filter(function (noPasa){
+    return  noPasa.promedio < 5;
+});
+console.log(desaprobados);
+
    
